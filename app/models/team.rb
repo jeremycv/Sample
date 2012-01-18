1a04eb3adc0db6ab2379e298cc17cd5cec731bd8
Jeremy Vandersay <jeremyv@tpg.com.au>
2012-01-18 19:48:55 +1100

    Retry

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,9 @@ class Team < ActiveRecord::Base
   
   include ApplicationHelper
   
-  before_save :calculate_status unless @team
+  unless @team.nil? 
+    before_save :calculate_status
+  end
   
   has_many :employees, :order => "name", :dependent => :destroy
   has_many :skills, :order => "name", :dependent => :destroy
